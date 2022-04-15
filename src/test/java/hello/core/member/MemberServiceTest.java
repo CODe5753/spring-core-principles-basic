88918c5b54ed0552b30f8f1f12a328a6635c372d
CODe1995 <code.57x53@gmail.com>
2022-04-15 23:44:41 +0900

    feat: appConfig로 관심사를 분리한다(의존관계 주입)
    - 각 구현체는 역할(interface)에만 의존한다
    - 각 구현체는 누가 주입되는지 상관하지 않는다
    - AppConfig를 통해 모든 구현체를 주입한다
    - 따라서 모든 Impl은 '실행'하는 책임만 지면 된다
    
    Resolve: #1

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,13 +1,23 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
+    @DisplayName("회원이 가입되면 ID로 찾을 수 있어야 한다")
     void join() {
         // given
         Member member = new Member(1L, "memberA", Grade.VIP);
